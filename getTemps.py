fd5097aa5fcf1d560315173ac06bbf627444efd3
drjnet <dave@drjnet.com>
2017-01-31 14:51:44 +0000

    Tidied up python and added web hook (post)support, added apiary definition to quickly spin up a mock api server.

--- getTemps.py
+++ getTemps.py
@@ -1,35 +1,63 @@
 import time
 import subprocess
 import os
+import requests
 
-# Clear screen
-os.system('clear')
+# Initialise
+def initialise():
+  os.system('modprobe w1-gpio')  # Activate GPIO module
+  os.system('modprobe w1-therm') # Activate Temperature module
+  os.system('clear') # clear screen
 
-# Set up find command
-findCMD = 'find /sys/devices/ -name "w1_slave"'
-out = subprocess.Popen(findCMD,shell=True,stdin=subprocess.PIPE, 
-                        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-# Get standard out and error
-(stdout, stderr) = out.communicate()
- 
-# Save found files to list
-filelist = stdout.decode().split()
+# scans through /sys/devices to find 1wire files (active sensors) and save results to list
+def findProbes(probes=''):
+  findCMD = 'find /sys/devices/ -name "w1_slave"'
+  out = subprocess.Popen(findCMD,shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+  # Get standard out and error
+  (stdout, stderr) = out.communicate()
+  probes = stdout.decode().split()
+  return (probes)
 
+# reads a file in filesystem
+def readFile(file):
+  tempfile = open(file)
+  thetext = tempfile.read()
+  tempfile.close()
+  return(thetext)
 
-#for probe in filelist:
-#    print "Probe found in : %s" % probe
-#quit()
+# post data to web hook (simple http post + params)
+def postTemp(key,probeID,temperature):
+  hdr = {'Content-Type': 'application/json'}
+  payload = {"key": key,"probeID": probeID,"temperature": temperature}
+  r = requests.post(postURL,params=payload)
+  print "Posting to API..."
+  print "Response - " + str(r.status_code)
+  print r.text
+  return
 
-while 1:
-	for probe in filelist:
-	    #tempfile = open("/sys/bus/w1/devices/28-00000620bd7a/w1_slave")
-	    tempfile = open(probe)	    
-	    thetext = tempfile.read()
-	    tempfile.close()
-	    probeid = probe.split("/")[4]
-	    tempdata = thetext.split("\n")[1].split(" ")[9]
-	    temperature = float(tempdata[2:])
-	    temperature = temperature / 1000
-	    print "Probe " + str(filelist.index(probe)+1) + " (id: " + probeid + ") Current Temperature is " + str(temperature) + "C"
-	time.sleep(5)
-	os.system('clear')
+# main program loop
+def mainLoop():
+  while 1:
+          for probe in probes:
+              probeData = readFile(probe)
+              probeID = probe.split("/")[4]
+              tempdata = probeData.split("\n")[1].split(" ")[9]
+              temperature = float(tempdata[2:]) / 1000
+              probeNum =  str(probes.index(probe)+1)
+              print "Probe " + probeNum + " (id: " + probeID + ") Current Temperature is " + str(temperature) + " C"
+              if apiPost:
+                postTemp(key,probeID,temperature)
+          time.sleep(5)
+          os.system('clear')
+
+# define vars
+probes = ''
+key='[api_key]'
+postURL='http://api_endpoint]'
+apiPost = True # change to false to disable api posting
+
+# main program
+initialise()
+probes=findProbes()
+mainLoop()
+
