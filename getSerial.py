230b9a329c34df132b572f9d54aa48d01c2b9de1
André Neto <andre.neto@ua.pt>
2017-04-16 15:39:25 +0100

    Remove leading zeros from serial; save/update params table on DB

--- getSerial.py
+++ getSerial.py
@@ -4,16 +4,23 @@
 #
 # Description : Grabs unique serial number from the Raspberry PI processor from /proc/cpuinfo, use so we know sensors are connect to this device.
 
+import re
+import sqlite3
+import settings
+
+DB_NAME = settings.DB_NAME
+
 ### Functions ###
 
 def getserial():
   # Extract serial from cpuinfo file
   cpuserial = "0000000000000000"
   try:
+    r = re.compile('0[0-9abcdef]+')
     f = open('/proc/cpuinfo','r')
     for line in f:
       if line[0:6]=='Serial':
-        cpuserial = line[10:26]
+        cpuserial = r.findall(line)[0].lstrip("0")
     f.close()
   except:
     cpuserial = "ERROR000000000"
@@ -21,4 +28,13 @@ def getserial():
   return cpuserial
 
 ### Program Loop ###
-print getserial()
+
+serial = getserial()
+print serial
+
+conn = sqlite3.connect(DB_NAME)
+c = conn.cursor()
+db_entry = ('PI_KEY', 'STRING', serial)
+c.execute("INSERT OR REPLACE INTO params (name,type,value) VALUES (?,?,?)", db_entry)
+conn.commit()
+conn.close()
