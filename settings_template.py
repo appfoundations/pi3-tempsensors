34d5299a17f35851161ccee99f4d7a6753a2fe25
André Neto <andre.neto@ua.pt>
2017-04-16 15:43:52 +0100

    New params added

--- settings_template.py
+++ settings_template.py
@@ -9,3 +9,21 @@ DELAY = 10 # number of seconds between readings (if running in loop mode)
 # API Settings
 URL = 'your api endpoint url'
 KEY = 'your api key'
+
+# DB Settings
+DB_NAME = "database.db" # DB name for measures and params
+SQL_FILE_NAME = "db_init.sql" # sql name to DB init
+
+# DHT Info
+DHT_V = 11      # DHT version '11': Adafruit_DHT.DHT11, '22': Adafruit_DHT.DHT22, '2302': Adafruit_DHT.AM2302 
+DHT_PIN = 25    # DHT connection pin
+
+# Software SPI configuration - MCP3008 ADC:
+# BCM pins numbers
+CLK  = 6
+MISO = 13
+MOSI = 19
+CS   = 26
+
+# Door sensors 
+DOOR_PINS = [18]    # array with door switch connections
